e57dd6889adcad3bafd4ef441463142dad24e73d
Chidozie Ugboaja <ugboajaemmanuelc@gmail.com>
2026-04-12 17:45:51 +0100

    feat: week1 day1 - variables and data typdes

---
+++ week1.js
@@ -0,0 +1,11 @@
+const clientName = "Acme Corp"  //
+const projectCount = 5  //
+const hourlyRate = 150.50   //
+const isActive = true   //
+const unpaidInvoice = null  //
+
+console.log(clientName)
+console.log(projectCount)
+console.log(hourlyRate)
+console.log(isActive)
+console.log(unpaidInvoice)
